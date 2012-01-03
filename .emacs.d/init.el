c782478582bfeb8f0a6bf27e19757fdf3e1bed15
Evan Callicoat <evan.callicoat@gmail.com>
2012-01-03 17:52:01 -0600

    Disable X-specific items, add xsel for X clipboard

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -17,10 +17,10 @@
 (menu-bar-mode -1)
 
 ;; Hide the toolbar
-(tool-bar-mode -1)
+;;(tool-bar-mode -1)
 
 ;; Hide the scrollbar
-(scroll-bar-mode -1)
+;;(scroll-bar-mode -1)
 
 ;; Region highlighting without key clobbering
 (cua-mode)
@@ -126,8 +126,8 @@
   (when (equal major-mode 'calendar-mode) ; Kill old calendars
     (kill-buffer))
   (when (string-match "\*.+\*" name)
-    (when (not (string-match "org\\|terminal\\|server\\|minibuf\\|scratch" name)) ; except
-      ;; (when (string-match "log\\|messages\\|completion\\|help\\|buffer" name)
+;;    (when (not (string-match "org\\|terminal\\|server\\|minibuf\\|scratch" name)) ; except
+    (when (string-match "log\\|messages\\|completion\\|help\\|buffer" name)
       (kill-buffer))))
 
 (defun kill-temp-buffers nil
@@ -143,7 +143,7 @@
       (kill-temp-buffer))))
 
 ;; Blows up on a few obscure things still, can't autorun all the time
-;;(run-at-time nil 10 'kill-temp-buffers)
+(run-at-time nil 10 'kill-temp-buffers)
 
 ;; Use keybinding instead
 (global-set-key (kbd "C-M-k") 'kill-temp-buffers)
@@ -154,7 +154,7 @@
           (lambda ()
             (org-indent-mode)))
 
-;; Set WM_URGENT hint!
+;; Set WM_URGENT hint if in X
 (defun x-urgency-hint (frame arg &optional source)
   (let* ((wm-hints (append (x-window-property "WM_HINTS" frame "WM_HINTS" source nil t) nil))
          (flags (car wm-hints)))
@@ -164,6 +164,31 @@
               (logand flags #xFFFFFEFF)))
     (x-change-window-property "WM_HINTS" wm-hints frame "WM_HINTS" 32 t)))
 
+;; Talk to xsel if in console
+(setq x-select-enable-clipboard t)
+(unless window-system
+ (when (getenv "DISPLAY")
+  ;; Callback for when user cuts
+  (defun xsel-cut-function (text &optional push)
+    ;; Insert text to temp-buffer, and "send" content to xsel stdin
+    (with-temp-buffer
+      (insert text)
+      (call-process-region (point-min) (point-max) "xsel" nil 0 nil "--clipboard" "--input")))
+  ;; Call back for when user pastes
+  (defun xsel-paste-function()
+    ;; Find out what is current selection by xsel. If it is different
+    ;; from the top of the kill-ring (car kill-ring), then return
+    ;; it. Else, nil is returned, so whatever is in the top of the
+    ;; kill-ring will be used.
+    (let ((xsel-output (shell-command-to-string "xsel --clipboard --output")))
+      (unless (string= (car kill-ring) xsel-output)
+        xsel-output )))
+  ;; Attach callbacks to hooks
+  (setq interprogram-cut-function 'xsel-cut-function)
+  (setq interprogram-paste-function 'xsel-paste-function)
+ ))
+
+;; Setup site-lisp
 (let ((default-directory "~/.emacs.d/site-lisp/"))
   (normal-top-level-add-to-load-path '("."))
   (normal-top-level-add-subdirs-to-load-path))
