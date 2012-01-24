e3235632f7ec7b18606cf08c49446631d42c229d
Evan Callicoat <evan.callicoat@gmail.com>
2012-01-23 20:41:27 -0600

    Disable auto bufferkill again

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -141,7 +141,7 @@
       (kill-temp-buffer))))
 
 ;; Blows up on a few obscure things still, can't autorun all the time
-(run-at-time nil 10 'kill-temp-buffers)
+;;(run-at-time nil 10 'kill-temp-buffers)
 
 ;; Use keybinding instead
 (global-set-key (kbd "C-M-k") 'kill-temp-buffers)
@@ -165,26 +165,26 @@
 ;; Talk to xsel if in console
 (setq x-select-enable-clipboard t)
 (unless window-system
- (when (getenv "DISPLAY")
-  ;; Callback for when user cuts
-  (defun xsel-cut-function (text &optional push)
-    ;; Insert text to temp-buffer, and "send" content to xsel stdin
-    (with-temp-buffer
-      (insert text)
-      (call-process-region (point-min) (point-max) "xsel" nil 0 nil "--clipboard" "--input")))
-  ;; Call back for when user pastes
-  (defun xsel-paste-function()
-    ;; Find out what is current selection by xsel. If it is different
-    ;; from the top of the kill-ring (car kill-ring), then return
-    ;; it. Else, nil is returned, so whatever is in the top of the
-    ;; kill-ring will be used.
-    (let ((xsel-output (shell-command-to-string "xsel --clipboard --output")))
-      (unless (string= (car kill-ring) xsel-output)
-        xsel-output )))
-  ;; Attach callbacks to hooks
-  (setq interprogram-cut-function 'xsel-cut-function)
-  (setq interprogram-paste-function 'xsel-paste-function)
- ))
+  (when (getenv "DISPLAY")
+    ;; Callback for when user cuts
+    (defun xsel-cut-function (text &optional push)
+      ;; Insert text to temp-buffer, and "send" content to xsel stdin
+      (with-temp-buffer
+        (insert text)
+        (call-process-region (point-min) (point-max) "xsel" nil 0 nil "--clipboard" "--input")))
+    ;; Call back for when user pastes
+    (defun xsel-paste-function()
+      ;; Find out what is current selection by xsel. If it is different
+      ;; from the top of the kill-ring (car kill-ring), then return
+      ;; it. Else, nil is returned, so whatever is in the top of the
+      ;; kill-ring will be used.
+      (let ((xsel-output (shell-command-to-string "xsel --clipboard --output")))
+        (unless (string= (car kill-ring) xsel-output)
+          xsel-output )))
+    ;; Attach callbacks to hooks
+    (setq interprogram-cut-function 'xsel-cut-function)
+    (setq interprogram-paste-function 'xsel-paste-function)
+    ))
 
 ;; Setup site-lisp
 (let ((default-directory "~/.emacs.d/site-lisp/"))
